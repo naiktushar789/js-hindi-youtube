7de5b6e0745cff29d453f0dcf5fb149e2bad7b64
Tushar Kant Naik <tusharkantnaik259@gmail.com>
2025-06-20 15:52:08 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=14002
+let accountEmail="tushar@gmail.com"
+var accountPass="12345"
+
+/*
+Prefer not to use var
+*/
+accountCity="Jaipur"
+
+//accountId=3 // not allowed
+accountEmail="wcwad@gmail.com"
+accountPass="23121"
+accountCity="Delhi"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity])
