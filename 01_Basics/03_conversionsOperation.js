0d3aba70e8c8b19dff985de3f4a69d761b13fd72
Tushar Kant Naik <tusharkantnaik259@gmail.com>
2025-06-20 16:35:41 +0000

    Operations in console

--- 01_Basics/03_conversionsOperation.js
+++ 01_Basics/03_conversionsOperation.js
@@ -7,3 +7,25 @@ console.log(typeof valueInNumber)
 //1=> true; 0=> False
 //""=> False
 //"Hello"=> True
+
+let number=33
+
+let snumber = String(number)
+console.log(snumber)
+console.log(typeof snumber)
+
+
+//----------------- Operations ----------------------
+
+let value = 3
+let negative = -value
+//console.log(negative)
+
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+
+console.log(+true)
+console.log(+"")
